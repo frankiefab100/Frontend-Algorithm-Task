361821106bcdc9ff20eabe4314d44c5be8ca3b9e
Franklin U.O. Ohaegbulam <46662771+frankiefab100@users.noreply.github.com>
2021-05-19 03:19:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 
+// Solution to Question 1
+const convertFahrToCelsius(value){
+if(value.type == number || value.type == "string" ){
+    let value = celsius * (9/5) + 32;
+    return value.toFixed(4);
+}
+
+else{
+    return `${value} is not a valid number but a/an ${value.type}`
+}
+}
+
+
+// Solution to Question 2
+function checkYuGiOh(n){
+    let arr = [];
+    if (n % 2 === 0 && n % 3 === 0 && n % 5 === 0){
+        arr.push("yu-gi-oh")
+    }
+    else if (n % 2 === 0 && n % 3 === 0){
+        arr.push("yu-gi")
+    }
+    else if (n % 2 === 0 && n % 5 === 0){
+        arr.push("yu-oh")
+    }
+    else if (n % 2 === 0){
+        arr.push("yu")
+    }
+    else if (n % 3 === 0){
+        arr.push("gi")
+    }
+    else if (n % 5 === 0){
+        arr.push("oh")
+    }
+    else{
+        return `invalid parameter`
+    }
+    return arr;
+}
